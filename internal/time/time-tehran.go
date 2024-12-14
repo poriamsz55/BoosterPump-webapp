68e99c956c6af90c6a4900217ee5fecb603a45ff
p.mousazadeh <p.mousazadeh@ravanertebat.com>
2024-12-15 01:13:21 +0330

    Fixed tehran time bug

--- internal/time/time-tehran.go
+++ internal/time/time-tehran.go
@@ -1,11 +1,16 @@
 package tehrantime
 
-import "time"
+import (
+	"time"
+	_ "time/tzdata" // Embed timezone database
+)
 
-// When retrieving, convert to Tehran time
 func FormattedDateTime(t1 time.Time) time.Time {
-	tehranLoc, _ := time.LoadLocation("Asia/Tehran")
+	tehranLoc, err := time.LoadLocation("Asia/Tehran")
+	if err != nil {
+		// Fallback to a fixed offset for Tehran (UTC+3:30)
+		tehranLoc = time.FixedZone("Asia/Tehran", 3*60*60+30*60)
+	}
 	tehranTime := t1.In(tehranLoc)
-
 	return tehranTime
 }
